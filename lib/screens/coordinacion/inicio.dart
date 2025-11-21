fffd4c8607b50481e19bab4eb574ce373bb44480
xJarucL <jarunyta1096@gmail.com>
2025-11-21 08:50:23 -0700

    Feat: Cargar Panel de Coordinador

--- lib/screens/coordinacion/inicio.dart
+++ lib/screens/coordinacion/inicio.dart
@@ -1,8 +1,13 @@
 // import 'dart:io';
 // import 'dart:typed_data';
+import 'dart:convert';
+
 import 'package:aula_idiomas_app/components/card-button.dart';
 // import 'package:aula_idiomas_app/screens/coordinacion/lista-docente.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 // import 'package:image_picker/image_picker.dart';
 // import 'package:flutter/foundation.dart';
 
@@ -16,7 +21,59 @@ class InicioCoordinacion extends StatefulWidget {
 }
 
 class _InicioCoordinacionState extends State<InicioCoordinacion> {
+  bool cargando = true;
+  
+  int totalGrupos = 0;
+  int totalDocentes = 0;
+  int totalAlumnos = 0;
+  int totalCoordinacion = 0;
+
   @override
+  void initState(){
+    super.initState();
+    cargarPanel();
+  }
+
+  Future<void> cargarPanel() async {
+    setState(() => cargando = true);
+
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('userToken') ?? '';
+
+    try {
+      final res = await http.get(
+        Uri.parse('${dotenv.env['API_URL']}${dotenv.env['API_INICIO_CORD']}'),
+        headers: {
+          "Accept": "application/json",
+          "Authorization": "Bearer $token",
+        }
+      );
+
+      if(res.statusCode == 200) {
+        final data = jsonDecode(res.body) as Map<String, dynamic>;
+
+        setState(() {
+          totalGrupos = data['gruposCount'];
+          totalDocentes = data['docentesCount'];
+          totalAlumnos = data['alumnosCount'];
+          totalCoordinacion = data['coordinadoresCount'];
+          cargando = false;
+        });
+      }else{
+        setState(() => cargando = false);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error al cargar panel (${res.statusCode})')),
+        );
+      }
+    } catch (e) {
+      setState(() => cargando = false);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error de conexión: $e')),
+      );
+    }
+
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
@@ -38,13 +95,6 @@ class _InicioCoordinacionState extends State<InicioCoordinacion> {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Text(
-                      'Coordinador/a, Jaruny Lupe Cardenas Tirado',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w300,
-                        color: Colors.grey[700],
-                      ),
-                    ),
                   ],
                 ),
               ),
@@ -64,26 +114,26 @@ class _InicioCoordinacionState extends State<InicioCoordinacion> {
                     physics: NeverScrollableScrollPhysics(),
                     children: [
                       CardButton(
-                        titulo: 'Docente',
-                        valor: 12,
+                        titulo: 'Docentes',
+                        valor: totalDocentes,
                         icono: Icons.person,
                         iconBackground: Colors.blue,
                       ),
                       CardButton(
                         titulo: 'Grupos',
-                        valor: 20,
+                        valor: totalGrupos,
                         icono: Icons.groups,
                         iconBackground: Colors.green,
                       ),
                       CardButton(
                         titulo: 'Alumnos',
-                        valor: 400,
+                        valor: totalAlumnos,
                         icono: Icons.school,
                         iconBackground: Colors.orange,
                       ),
                       CardButton(
-                        titulo: 'Accesos',
-                        valor: 3,
+                        titulo: 'Coordinadores',
+                        valor: totalCoordinacion,
                         icono: Icons.key,
                         iconBackground: Colors.red,
                       ),
